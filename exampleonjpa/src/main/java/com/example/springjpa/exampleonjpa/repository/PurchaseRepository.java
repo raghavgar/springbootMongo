4f51cfe04ce6abd0a490cf96772c459016d1fc8c
raghavendra1221 <raghavendra12@icloud.com>
2020-05-30 21:17:29 -0400

    Code to read from DB and Writing using JPA One to many mapping

--- exampleonjpa/src/main/java/com/example/springjpa/exampleonjpa/repository/PurchaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springjpa.exampleonjpa.repository;
-
-import com.example.springjpa.exampleonjpa.model.Purchase;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PurchaseRepository extends CrudRepository<Purchase, String> {
-}
